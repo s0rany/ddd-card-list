7e581363884fa629a9b47d43f46211479845674c
s0rany <sfc5954@psu.edu>
2025-03-30 14:48:18 -0400

    Update ddd-card.js

--- ddd-card.js
+++ ddd-card.js
@@ -1,16 +1,8 @@
-/**
- * Copyright 2025 s0rany
- * @license Apache-2.0, see LICENSE for full text.
- */
+
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
-/**
- * `ddd-card`
- * 
- * @demo index.html
- * @element ddd-card
- */
+
 export class DddCard extends DDDSuper(I18NMixin(LitElement)) {
    
     static get tag() {
